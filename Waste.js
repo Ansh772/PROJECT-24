cb48a081ef8e4e2a940c485ce527dfc3af664333
Ansh772 <73747183+Ansh772@users.noreply.github.com>
2020-11-20 20:54:11 +0530

    Add files via upload

---
+++ Waste.js
@@ -0,0 +1,25 @@
+class Waste{
+
+    constructor(x, y) {
+        var options = {
+            'isStatic':false,
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2
+        }
+        this.body = Bodies.rectangle(x, y, 20,20, options);
+        this.width = 20;
+        this.height = 20;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos =this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        strokeWeight(4);
+        stroke("red");
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+      }
+    };  
